a9c0626e992c82fdaa17caf1eedbb23e08ebcf03
Raymond Shiner <raymondshiner@gmail.com>
2023-08-21 15:20:03 -0700

    main content wrapper in place

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { TopBar } from "./TopBar"
+import { styled } from "styled-components"
+import { BlogPost } from "./components/BlogPost"
+import { TopBar } from "./components/TopBar"
 
 const App = () => {
-    return <TopBar />
+    return <>
+        <TopBar />
+        <MainContentWrapper>
+            <BlogPost />
+        </MainContentWrapper>
+    </>
 }
 
 export default App
+
+const MainContentWrapper = styled.div`
+    //component
+    box-sizing: border-box;
+    height: calc(100vh - 48px);
+    margin-top: 48px;
+    
+    //content
+    padding: 24px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`;
