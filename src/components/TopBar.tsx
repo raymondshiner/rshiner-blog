420e1d63acf5fb2efe18337d1d083a11c8cea73a
Raymond Shiner <raymondshiner@gmail.com>
2023-08-21 15:11:50 -0700

    move files to components folder

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { GithubIcon, LinkedInIcon, WebsiteIcon } from "./icons";
+import { GithubIcon, LinkedInIcon, WebsiteIcon } from "../icons";
 
 export const TopBar = () => {
     return <MainWrapper>
