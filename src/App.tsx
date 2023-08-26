db3b6b2f99949988b197275fa50713bf38c8618c
Raymond Shiner <raymondshiner@gmail.com>
2023-08-26 12:41:29 -0700

    rework app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,18 @@ import { PageNotFound404 } from "./components/404";
 import { BlogPostList } from "./components/BlogPostList/BlogPostList";
 import { TopBar } from "./components/TopBar/TopBar";
 
+const MainContentWrapper = styled.div`
+    //component
+    box-sizing: border-box;
+    margin-top: 48px;
+    height: calc(100vh - 48px);
+    width: 100%;
+    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
 const AppLayout = () => <>
     <TopBar />
     <MainContentWrapper>
@@ -24,20 +36,4 @@ const router = createBrowserRouter([
     },
 ])
 
-const App = () => <>
-    <RouterProvider router={router} />
-</>
-
-export default App
-
-const MainContentWrapper = styled.div`
-    //component
-    box-sizing: border-box;
-    margin-top: 48px;
-    height: calc(100vh - 48px);
-    width: 100%;
-    overflow-y: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`;
+export default function App() { return <RouterProvider router={router} /> }
