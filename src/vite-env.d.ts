2ff0f7a28756a6be6a8af9fd81d57b024f9878d6
Raymond Shiner <raymondshiner@gmail.com>
2023-08-26 17:57:52 -0700

    remove old deps

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaENV {
-    readonly VITE_DROPBOX_CLIENT_ID: string;
-    readonly VITE_DROPBOX_CLIENT_SECRET: string;
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv
-}
